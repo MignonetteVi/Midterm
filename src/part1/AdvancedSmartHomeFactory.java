dd46cbca846e96ad2c547ffd4edfb5b417cd8546
Kristina <tolstovachristina@gmail.com>
2025-03-30 22:43:03 +0500

    completed AdvancedSmartHomeFactory

--- src/part1/AdvancedSmartHomeFactory.java
+++ src/part1/AdvancedSmartHomeFactory.java
@@ -1,19 +1,28 @@
 package part1;
 
+// Расширенная фабрика
 class AdvancedSmartHomeFactory implements SmartHomeFactory {
 
     @Override
     public Light createLight(String name) {
-        return null;
+        return new Light(name);
     }
 
     @Override
     public Camera createCamera(String name) {
-        return null;
+        return new Camera(name);
     }
 
     @Override
     public RobotVacuum createRobotVacuum(String name) {
-        return null;
+        return new RobotVacuum(name);
+    }
+
+    public AIThermostat createAIThermostat(String name) {
+        return new AIThermostat(name);
+    }
+
+    public MotionSensor createMotionSensor(String name) {
+        return new MotionSensor(name);
     }
 }
