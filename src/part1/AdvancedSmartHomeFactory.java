32e25f1427cdb47a618d19ec52add3b8668fae45
Kristina <tolstovachristina@gmail.com>
2025-03-30 22:37:30 +0500

    AdvancedSmartHomeFactory
    Class in Progress

---
+++ src/part1/AdvancedSmartHomeFactory.java
@@ -0,0 +1,19 @@
+package part1;
+
+class AdvancedSmartHomeFactory implements SmartHomeFactory {
+
+    @Override
+    public Light createLight(String name) {
+        return null;
+    }
+
+    @Override
+    public Camera createCamera(String name) {
+        return null;
+    }
+
+    @Override
+    public RobotVacuum createRobotVacuum(String name) {
+        return null;
+    }
+}
