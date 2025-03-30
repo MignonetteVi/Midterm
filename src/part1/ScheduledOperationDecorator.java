0197366de5498ab9760c5250d4a9ea1c67f725d8
Kristina <tolstovachristina@gmail.com>
2025-03-30 22:06:21 +0500

    completed SheduledOperationDecorator

--- src/part1/ScheduledOperationDecorator.java
+++ src/part1/ScheduledOperationDecorator.java
@@ -1,8 +1,26 @@
 package part1;
 
+import java.time.LocalTime;
+
+// Интерфейс умного устройства
 class ScheduledOperationDecorator extends SmartDeviceDecorator {
+    private LocalTime startTime;
+    private LocalTime endTime;
 
     public ScheduledOperationDecorator(SmartDevice device) {
         super(device);
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public void turnOn() {
+        LocalTime now = LocalTime.now();
+        if (now.isAfter(startTime) && now.isBefore(endTime)) {
+            System.out.println("shedule!: Сейчас подходящее время — устройство включается.");
+            super.turnOn();
+        } else {
+            System.out.println("shedule!: Вне времени расписания — устройство не включается.");
+        }
     }
 }
