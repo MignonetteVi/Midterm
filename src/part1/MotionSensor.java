4b2d34780137905f26662aad722dc694e121cf71
Kristina <tolstovachristina@gmail.com>
2025-03-30 22:28:28 +0500

    completed MotionSensor

--- src/part1/MotionSensor.java
+++ src/part1/MotionSensor.java
@@ -1,19 +1,34 @@
 package part1;
 
+// Продвинутое устройство: Датчик движения
 class MotionSensor implements SmartDevice {
+    private String name;
+    private boolean motionDetected = false;
+
+    public MotionSensor(String name) {
+        this.name = name;
+    }
+
+    public boolean isMotionDetected() {
+        return motionDetected;
+    }
+
+    public void simulateMotion() {
+        motionDetected = true;
+    }
 
     @Override
     public void turnOn() {
-
+        System.out.println(name + " активирован (датчик движения!.");
     }
 
     @Override
     public void turnOff() {
-
+        System.out.println(name + " деактивирован (датчик движения!).");
     }
 
     @Override
     public String getStatus() {
-        return "";
+        return name + ": Обнаружение движения — " + (motionDetected ? "зафиксировано" : "отсутствует");
     }
 }
