083c2228fbde19eb810b67c311264eb977ba8eb4
Kristina <tolstovachristina@gmail.com>
2025-03-30 23:04:01 +0500

    completed LegacyDoorLockAdapter

--- src/part1/LegacyDoorLockAdapter.java
+++ src/part1/LegacyDoorLockAdapter.java
@@ -1,19 +1,25 @@
 package part1;
 
+// Адаптер: для легаси замка двери
 class LegacyDoorLockAdapter implements SmartDevice {
+    private LegacyDoorLock lock;
+
+    public LegacyDoorLockAdapter(LegacyDoorLock lock) {
+        this.lock = lock;
+    }
 
     @Override
     public void turnOn() {
-        
+        lock.unlockDoor();
     }
 
     @Override
     public void turnOff() {
-
+        lock.lockDoor();
     }
 
     @Override
     public String getStatus() {
-        return "";
+        return "Замок двери: " + (lock.isLocked() ? "Заблокирован" : "Разблокирован");
     }
 }
