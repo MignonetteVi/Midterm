71ead4d34134712e3197a8c0deba1560a0110920
Kristina <tolstovachristina@gmail.com>
2025-03-30 21:33:16 +0500

    SmartDeviceDecorator
    Class in Progress

---
+++ src/part1/SmartDeviceDecorator.java
@@ -0,0 +1,18 @@
+package part1;
+
+abstract class SmartDeviceDecorator implements SmartDevice {
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+
+    @Override
+    public String getStatus() {
+        return "";
+    }
+}
