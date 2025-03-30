67432ed9639d2fd4eeae33baafb4fed852bbe659
Kristina <tolstovachristina@gmail.com>
2025-03-30 22:18:38 +0500

    completed AIThermostat

--- src/part1/AIThermostat.java
+++ src/part1/AIThermostat.java
@@ -1,19 +1,31 @@
 package part1;
 
+// Продвинутое устройство: AI-Термостат
 class AIThermostat implements SmartDevice {
+    private int temperature;
+    private String name;
+
+    public AIThermostat(String name) {
+        this.name = name;
+    }
+
+    public void setTemperature(int temp) {
+        this.temperature = temp;
+        System.out.println(name + ": Температура установлена на " + temp + "°C");
+    }
 
     @Override
     public void turnOn() {
-
+        System.out.println(name + " включён.");
     }
 
     @Override
     public void turnOff() {
-
+        System.out.println(name + " выключен.");
     }
 
     @Override
     public String getStatus() {
-        return "";
+        return name + ": Температура " + temperature + "°C";
     }
 }
