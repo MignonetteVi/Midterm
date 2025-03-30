389906a5bc522476cf4b9cbef6e5d310d8c0f431
Kristina <tolstovachristina@gmail.com>
2025-03-30 22:10:19 +0500

    AIThermostat
    Class in Progress

---
+++ src/part1/AIThermostat.java
@@ -0,0 +1,19 @@
+package part1;
+
+class AIThermostat implements SmartDevice {
+
+    @Override
+    public void turnOn() {
+
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+
+    @Override
+    public String getStatus() {
+        return "";
+    }
+}
