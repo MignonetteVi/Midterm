a4e07c0abdb6d37b0a6378ed28e1162c2a60af6d
Kristina <tolstovachristina@gmail.com>
2025-03-30 22:55:12 +0500

    LegacyDoorLockAdapter
    Class in Progress

---
+++ src/part1/LegacyDoorLockAdapter.java
@@ -0,0 +1,19 @@
+package part1;
+
+class LegacyDoorLockAdapter implements SmartDevice {
+
+    @Override
+    public void turnOn() {
+        
+    }
+
+    @Override
+    public void turnOff() {
+
+    }
+
+    @Override
+    public String getStatus() {
+        return "";
+    }
+}
