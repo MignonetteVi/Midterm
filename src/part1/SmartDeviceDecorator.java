93d89cec75fdd8285a92afa141c29cbcb6e95931
Kristina <tolstovachristina@gmail.com>
2025-03-30 21:39:03 +0500

    completed SmartDeviceDecorator

--- src/part1/SmartDeviceDecorator.java
+++ src/part1/SmartDeviceDecorator.java
@@ -1,18 +1,25 @@
 package part1;
 
+// Декоратор: базовый абстрактный класс
 abstract class SmartDeviceDecorator implements SmartDevice {
+    protected SmartDevice device;
+
+    public SmartDeviceDecorator(SmartDevice device) {
+        this.device = device;
+    }
+
     @Override
     public void turnOn() {
-
+        device.turnOn();
     }
 
     @Override
     public void turnOff() {
-
+        device.turnOff();
     }
 
     @Override
     public String getStatus() {
-        return "";
+        return device.getStatus();
     }
 }
