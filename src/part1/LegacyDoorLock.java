24168bb84aac349dedea52dfdf2b328891206005
Kristina <tolstovachristina@gmail.com>
2025-03-30 22:48:01 +0500

    completed LegacyDoorLock

--- src/part1/LegacyDoorLock.java
+++ src/part1/LegacyDoorLock.java
@@ -1,4 +1,15 @@
 package part1;
 
+// Адаптер(абстракция для примера): старый замок двери
 class LegacyDoorLock {
+    boolean locked = false;
+    public void unlockDoor() {
+        System.out.println("Замок разблокирован (устаревшая система!)");
+        locked = false;
+    }
+
+    public void lockDoor() {
+        System.out.println("Замок заблокирован (устаревшая система!)");
+        locked = true;
+    }
 }
