e66b8766bc079e89d02a80d01c15dd6744c6faa1
Kristina <tolstovachristina@gmail.com>
2025-03-30 21:44:08 +0500

    completed LoggingDecorator

--- src/part1/LoggingDecorator.java
+++ src/part1/LoggingDecorator.java
@@ -1,8 +1,19 @@
 package part1;
 
+// Декоратор: логирование
 class LoggingDecorator extends SmartDeviceDecorator {
 
     public LoggingDecorator(SmartDevice device) {
         super(device);
+    }
+
+    @Override
+    public void turnOn() {
+        super.turnOn();
+    }
+
+    @Override
+    public void turnOff() {
+        super.turnOff();
     }
 }
